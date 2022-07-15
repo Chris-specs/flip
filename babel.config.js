3eb25b67de7c4e52aeaae563834307c6907fadcb
Christian Sanchez <christian.s.molina@gmail.com>
2022-07-15 15:26:55 -0500

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+    api.cache(true)
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    root: ['./app'],
+                    alias: {
+                        '@screens': './app/screens',
+                        '@components': './app/components',
+                        '@services': './app/services',
+                        '@hooks': './app/hooks/index.js',
+                        '@navigation': './app/navigations/index.js',
+                        '@utils': './app/utils/index.js'
+                    },
+                },
+            ],
+        ],
+    }
+}
